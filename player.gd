94726ed0598fdd46f385a4768d2e69d6fb53b4d5
Egor Bocharov <54062133+sof4k1n@users.noreply.github.com>
2026-04-12 18:32:36 +0500

    Add files via upload

---
+++ player.gd
@@ -0,0 +1,43 @@
+extends CharacterBody3D
+
+
+const SPEED = 5.0
+const SPRINT_SPEED = 8.0
+const JUMP_VELOCITY = 5.0
+const DOUBLE_JUMP_VELOCITY = 4.0
+const MOUSE_SENSITIVITY = 0.003
+var has_double_jamped = false
+var gravity = ProjectSettings.get_setting('physics/3d/default_gravity')
+
+@onready var spring_arm = $SpringArm3D
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * MOUSE_SENSITIVITY)
+		$Camera3D.rotate_x(-event.relative.y * MOUSE_SENSITIVITY)
+		$Camera3D.rotation.x = clamp($Camera3D.rotation.x, deg_to_rad(-89), deg_to_rad(89))
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
